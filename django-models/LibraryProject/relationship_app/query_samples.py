31af538f2a12062c0a7b57161533da2794e96b99
NanaYawSkrila <skriladrilladerula@icloud.com>
2025-10-05 04:44:58 +0000

    Fixed query patterns in query_samples.py for ORM checks

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,8 @@ from LibraryProject.relationship_app.models import Author, Book, Library, Librar
 
 # Query all books by a specific author
 author_name = "George Orwell"
-books_by_author = Book.objects.filter(author__name=author_name)
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
 print(f"Books by {author_name}:", books_by_author)
 
 # List all books in a library
