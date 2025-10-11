2639cfe3e97ebd2c66626453b8c62dca44638d12
NanaYawSkrila <skriladrilladerula@icloud.com>
2025-10-11 11:16:04 +0000

    Add views for BookListView and LibraryDetailView with templates

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from relationship_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    path('books/', views.BookListView.as_view(), name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
