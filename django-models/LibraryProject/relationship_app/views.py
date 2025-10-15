b70b26a6fa5a748fedae1eff957d429112bdea28
NanaYawSkrila <141090270+NanaYawSkrila@users.noreply.github.com>
2025-10-15 22:28:42 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,39 @@ from .models import Library, Book
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
+
+# --- Helper functions to check user roles ---
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+# --- Admin View ---
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+# --- Librarian View ---
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+# --- Member View ---
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
 def register_view(request):
@@ -84,5 +116,6 @@ def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
 PY
+
 
 
